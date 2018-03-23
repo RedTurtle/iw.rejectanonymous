757442017c13a525c9d82aecde5e7814b0448cd5
Giacomo Monari <arsenico13@hotmail.it>
2018-03-23 12:21:11 +0100

    [chg] browser layer interface, change name: IIwRejectanonymousLayer

--- iw/rejectanonymous/interfaces.py
+++ iw/rejectanonymous/interfaces.py
@@ -4,5 +4,5 @@
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 
-class IwRejectAnonymousLayer(IDefaultBrowserLayer):
+class IIwRejectanonymousLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
