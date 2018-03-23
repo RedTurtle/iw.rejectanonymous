8dc2aba9f0db70770968396f1d269df20c1a1c2a
Giacomo Monari <arsenico13@hotmail.it>
2018-03-23 12:19:42 +0100

    [chg][dev] setup.py - set the version to 2.0.0.dev0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(*names):
     path = os.path.join(here, *names)
     return open(path, 'r').read().strip()
 
-version = '1.2.4.dev0'
+version = '2.0.0.dev0'
 
 setup(name='iw.rejectanonymous',
       version=version,
