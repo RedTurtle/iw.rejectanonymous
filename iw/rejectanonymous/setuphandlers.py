be5424f841b89c4a1d9d5f2a00f8eeef0944c4be
Giacomo Monari <arsenico13@hotmail.it>
2018-03-28 12:08:50 +0200

    [fix] uninstall profile: cleaning up registry, control panel and IPrivateSite interfaces

--- iw/rejectanonymous/setuphandlers.py
+++ iw/rejectanonymous/setuphandlers.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+from iw.rejectanonymous import IPrivateSite
+from iw.rejectanonymous import logger
+from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
+from Products.Five.utilities.marker import erase
 from zope.interface import implementer
 
 
@@ -20,4 +24,7 @@ def post_install(context):
 
 def uninstall(context):
     """Uninstall script"""
+    site = api.portal.get()
+    erase(site, IPrivateSite)
+    logger.info("iw.rejectanonymous:uninstall - The site has been set public.")
     # Do something at the end of the uninstallation of this package.
